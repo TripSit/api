4404ec20530d11ddacbf2397cfc42800e23aea67
SevenCats <snowcolton@hotmail.com>
2021-01-19 23:45:08 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+	root: true,
+	extends: 'airbnb-base',
+	parserOptions: { sourceType: 'script' },
+	env: { node: true },
+	rules: {
+		strict: [2, 'global'],
+		indent: [2, 'tab'],
+		'no-tabs': 0,
+		'arrow-parens': 0,
+		'consistent-return': 0,
+	},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.js',
+				'**/__mocks__/*.js',
+				'tests/**/*.spec.js',
+				'jest.setup.js',
+			],
+			plugins: ['jest'],
+			extends: ['plugin:jest/all'],
+			env: { 'jest/globals': true },
+			rules: {
+				'jest/no-hooks': 0,
+				'jest/prefer-expect-assertions': 0,
+				'jest/no-test-return-statement': 0,
+				'jest/require-top-level-describe': 0,
+				'jest/expect-expect': 0,
+				'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+			},
+		},
+	],
+};
